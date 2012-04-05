0f95558c828bc251c3561054600738fc73189060
Rob Ludwick <rcludw@gmail.com>
2012-04-05 02:37:37 -0400

    Added pavement.py

---
+++ pavement.py
@@ -0,0 +1,12 @@
+from paver.easy import *
+import paver.doctools
+from paver.setuputils import setup
+
+setup(
+        name="Python Cheat Sheets",
+        packages=['pcs'],
+        version="0.01",
+        url="http://github.com/rcludwick/pcs",
+        author="Rob Ludwick",
+        author_email="someone@somewhere"
+        )
