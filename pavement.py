241dc079107f10fad5e4f40c1ecac46dba1a8347
Rob Ludwick <rcludw@gmail.com>
2012-04-07 03:56:45 -0400

    Started writing pavement.py.
    Moved threading.rst into a src dir.

--- pavement.py
+++ pavement.py
@@ -8,5 +8,45 @@ setup(
         version="0.01",
         url="http://github.com/rcludwick/pcs",
         author="Rob Ludwick",
-        author_email="someone@somewhere"
+        author_email="rcludw_gmail"
         )
+
+docroot = path("./pavement.rst").abspath().dirname() / "doc"
+
+options(
+        docroot = docroot,
+        tmpdir = docroot / "tmp",
+        srcdir = docroot / "src",
+        builddir = docroot / "tmp" / "pdf", 
+        srcbuilddir = docroot / "tmp" / "src",
+        )
+
+options(
+        rst_ext = ".rst",
+        )
+
+options(
+        rst2pdf_stylesheets = ("twocolumn","letter","eightpoint","freetype-sans","friendly",),
+        )
+
+@task
+def create_docdir(options):
+    if not options.docroot.exists():
+        options.docroot.mkdir()
+
+@task
+@needs('create_docdir')
+def clean_tmp(options):
+    if options.tmpdir.exists():
+        options.tmpdir.rmtree()
+    options.tmpdir.mkdir()
+
+@task
+@needs('clean_tmp')
+def copy_src_to_srcbuild(options):
+    srcfiles = srcdir / glob("*" + options.rst_ext) 
+    srcfiles.copy(options.srcbuilddir)
+
+    
+
+    
